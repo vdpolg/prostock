1488a61b95d002268542f91f613fc1b23184a606
vdpolg <vdpolg@gmail.com>
2018-09-04 22:25:08 +0800

    股名、代號塞入stock.q1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # prostock
+#名稱           版本    日期            作者    備註
+#prostock       v0.1    20180903        arthur  先抓資料：元大0050
+#prostock       v0.2	20180904				初步能撈第17筆資料的股名、代號塞入stock.q1 、也能欄轉列
+												待處理for loop 
+<pre>
+
+        # </td> 結尾變斷行          # 股票代號和股名中間改@分隔 	#代號前的字清空       #只撈出 @ 的行
+cat p | sed 's/\/td>/\/td> \n /g' 	|sed 's/.htm.>/ @ /g'          |sed 's/.*ofile.//g' |grep @
+
+</pre>
--- prostock.sh
+++ prostock.sh
@@ -2,9 +2,18 @@
 #名稱		版本	日期		作者	備註
 #prostock	v0.1	20180903	arthur	先抓資料：元大0050
 STOCKTMP=/tmp/stock.tmp #暫存檔
+STOCK_Q=/tmp/stock.q #暫存檔
 curl -sk https://www.cnyes.com/twstock/Etfingredient/0050.htm > $STOCKTMP
-grep 台泥 $STOCKTMP
+grep 台泥 $STOCKTMP| sed 's/\/td>/\/td> \n /g' |sed 's/.htm.>/ @ /g' |sed 's/.*ofile.//g' |grep @ |sed 's/<.*>//g' > stock.q1
+#欄轉列 #0905待處理(for loop)
+cat stock.q1 |awk '{print $1}'|xargs
 #取得關鍵字之前 1101.htm的資料，點進去後先抓成交價再塞csv檔令後續DB科科
 # 0.Primary key 1.股票名稱	2.代號	3.收盤後成交價 4.當日最高 5.當日最低
 # 待處理：塞進來的文字檔易於閱讀
+stock_query(){
+	Q=$QUERY
+	curl -sk https://www.cnyes.com/twstock/Etfingredient/${Q}.htm > $STOCK_Q
+}
+#stock_query $QUERY
+# cat stock.tmp |grep 台泥 | sed 's/\/td>/\/td> \n /g' |sed 's/.htm.>/ @ /g' |sed 's/.*ofile.//g' |grep @
 
